596c3026c481e3d48db85bb5f514e22b238350e5
PkSnoovie <166737867+PkSnoovie@users.noreply.github.com>
2025-10-02 13:33:21 -0400

    Player Movement
    Basic Player Movement and Camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+
+    CharacterController controller;
+    public float speed = 3f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized; //.normalize stops player from moving faster diagonally
+
+        //if (movement.magnitude >= 0.1f)
+        //{
+        controller.Move(movement * speed * Time.deltaTime);
+        //}
+    }
+}
