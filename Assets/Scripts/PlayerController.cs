9bb4a34e9be17af6c027e7091c4e70fcb46d7ef5
PkSnoovie <166737867+PkSnoovie@users.noreply.github.com>
2025-10-07 13:54:07 -0400

    Basic animations for player
    Player can now look in different directions when moving (but they just slide against the floor).

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,26 @@ public class PlayerController : MonoBehaviour
     CharacterController controller;
     public float speed = 3f;
 
+    private float prevX;
+    private float prevZ;
+
+    [SerializeField] private Sprite player_down;
+    [SerializeField] private Sprite player_left;
+    [SerializeField] private Sprite player_right;
+    [SerializeField] private Sprite player_up;
+    private SpriteRenderer playerSprite;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         controller = GetComponent<CharacterController>();
+        playerSprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        prevX = transform.position.x;
+        prevZ = transform.position.z;
     }
 
     // Update is called once per frame
@@ -24,9 +40,27 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized; //.normalize stops player from moving faster diagonally
 
-        //if (movement.magnitude >= 0.1f)
-        //{
         controller.Move(movement * speed * Time.deltaTime);
-        //}
+
+        if (transform.position.z > prevZ)
+        {
+            playerSprite.sprite = player_up;
+        }
+        if (transform.position.x > prevX)
+        {
+            playerSprite.sprite = player_right;
+        }
+        
+        if (transform.position.x < prevX)
+        {
+            playerSprite.sprite = player_left;
+        }
+        if (transform.position.z < prevZ)
+        {
+            playerSprite.sprite = player_down;
+        }
+
+        prevX = transform.position.x;
+        prevZ = transform.position.z;
     }
 }
