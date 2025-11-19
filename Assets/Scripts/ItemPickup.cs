ba1d9b290d2a2a01891965949da35ca5b98e8f92
pegasiperspectives <113609176+pegasiperspectives@users.noreply.github.com>
2025-11-18 23:30:24 -0500

    added placeholder mom and dad to scene

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -29,10 +29,17 @@ public class ItemPickup : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (Physics.Raycast(camera.transform.position, camera.transform.TransformDirection(Vector3.up), out hit, Mathf.Max(5)))
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 50))
+        {
+            Pickup();
+            Debug.Log("iT WORKED");
+        }
+        
+        /*if (Physics.Raycast(camera.transform.position, camera.transform.TransformDirection(Vector3.up), out hit, Mathf.Max(5)))
         {
             Pickup();
             Debug.Log("");
-        }
+        }*/
     }
 }
