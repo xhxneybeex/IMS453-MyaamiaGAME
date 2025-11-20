020c981974f5c593ed00261bba467557e9266101
MichaelSlackMiami <slackmg@miamioh.edu>
2025-11-19 20:47:22 -0500

    Escape the House

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -35,7 +35,7 @@ public class SceneController : MonoBehaviour
 
         if (this.name == "Rug Door")
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Level_1_Exterior");
+            SceneManager.LoadScene("Level_1_Exterior");
             Debug.Log("MMMM");
             is2DScene = false;
         }
