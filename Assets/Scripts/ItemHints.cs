0d3203fe6ce117575cd3072393055edac8963f6c
PkSnoovie <166737867+PkSnoovie@users.noreply.github.com>
2025-12-02 21:31:49 -0500

    added map to hud

--- Assets/Scripts/ItemHints.cs
+++ Assets/Scripts/ItemHints.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class ItemHints : MonoBehaviour
 {
     public DialogueManager dm;
-    public int glovesHintAdded = 0;
+    public bool glovesHintAdded = false;
+    public bool coatHintAdded = false;
+
+    public Item item;
 
 
     public void Start()
@@ -13,15 +16,25 @@ public class ItemHints : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("you entered a task update trigger");
-        if (this.name.Equals("GlovesTaskUpdateTrigger") && glovesHintAdded == 0)
+        //if (this.name.Equals("GlovesTaskUpdateTrigger") && glovesHintAdded == 0)
+        if (item.id == 6)
         {
             Debug.Log("you entered the trigger for gloves");
             if (dm.talkedToMom == true)
             {
-                dm.tasksInBook = dm.tasksInBook.Replace("mom needs me to find my alencihkana\n", "You walked by the alencihkana\n");
+                dm.tasksInBook += "\nYou walked by the alencihkana\n";
                 Debug.Log("it should be updating tasksinbook now");
             }
-            glovesHintAdded = 1;
+            glovesHintAdded = true;
+        } else if (item.id == 7)
+        {
+            Debug.Log("You entered the trigger for the coat");
+            if (dm.talkedToDad == true)
+            {
+                dm.tasksInBook += "\nYou walked by the keehpakiikinki naapinaakani\n";
+                Debug.Log("it should be updating tasksinbook now");
+            }
+            coatHintAdded = true;
         }
     }
 
