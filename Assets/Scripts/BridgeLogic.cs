f36cfcbdb4d4347e67b87092330619504d21928d
MichaelSlackMiami <slackmg@miamioh.edu>
2025-12-10 13:33:52 -0500

    Fixes

---
+++ Assets/Scripts/BridgeLogic.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class BridgeLogic : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (InventoryManager.GlovesCollected && InventoryManager.CoatCollected && InventoryManager.RugCollected && InventoryManager.HammerCollected
+            && InventoryManager.SaltCollected && InventoryManager.PepperCollected && InventoryManager.CoffeeCollected && InventoryManager.KeysCollected
+            && InventoryManager.BrushCollected && InventoryManager.BlanketCollected && InventoryManager.ChairCollected && InventoryManager.ShoeCollected)
+        {
+            gameObject.SetActive(true);
+        } else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
