39e92c0c2b918a97a07de5192e82944b51f33ae6
pegasiperspectives <113609176+pegasiperspectives@users.noreply.github.com>
2025-12-10 11:50:40 -0500

    updated pause menu

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -20,7 +20,7 @@ public class SceneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter()
@@ -35,20 +35,21 @@ public class SceneController : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("Interior_PlayerHouse");
     }
 
-    public void YesExit() {
-        Application.Quit();
+    public void YesExit()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("StartMenu");
     }
 
     public void NoExit()
     {
-      //  exitScreen.SetActive(false);
+        exitScreen.SetActive(false);
     }
 
     public void OpenSettings()
     {
         settingsScreen.SetActive(true);
     }
-    
+
     public void CloseSettings()
     {
         settingsScreen.SetActive(false);
